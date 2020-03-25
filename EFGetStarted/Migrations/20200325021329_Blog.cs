193d320610c238bd2d80215b387713739b1e185d
Mantaroh Yoshinaga <mantaroh@gmail.com>
2020-03-25 11:29:05 +0900

    MigrationType 追加

---
+++ EFGetStarted/Migrations/20200325021329_Blog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFGetStarted.Migrations
+{
+    public partial class Blog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# EF Core勉強会用レポジトリ
+
+## Getting Started
+
+### Migration Type
+
+```
+$ cd EFGetStarted
+$ dotnet ef database update
+$ dotnet run
+```
